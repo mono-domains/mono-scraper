a35f08e9be9142a2578cfe155a7852c5db1c5f35
Josh <hello@josh.ee>
2023-04-15 13:27:42 +0100

    Add Netlify build hook

--- src/handlers/FrontEndDeploymentHandler.js
+++ src/handlers/FrontEndDeploymentHandler.js
@@ -1,7 +1,32 @@
+const axios = require('axios')
+
 const SSHConnection = require('../connections/SSHConnection')
 
 class FrontEndDeploymentHandler {
   async deployFrontEnd() {
+    try {
+      await this.deployFrontEndNetlify()
+    } catch (e) {
+      try {
+        await this.deployFrontEndSSH()
+      } catch (e) {
+        throw new Error('No front end deployment method set')
+      }
+    }
+  }
+
+  async deployFrontEndNetlify() {
+    if (!process.env.NETLIFY_BUILD_HOOK_URL) {
+      throw new Error('Netlify Build Hook URL not set')
+    }
+
+    await axios({
+      url: process.env.NETLIFY_BUILD_HOOK_URL,
+      method: 'POST'
+    })
+  }
+
+  async deployFrontEndSSH() {
     // Create a new connection to the FE server
     const ssh = new SSHConnection()
     const connection = await ssh.getConnection()
